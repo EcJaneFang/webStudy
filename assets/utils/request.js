b332cdcb36fbb57fe0db6a93f1433b95f1315c25
詹方 <804571770@qq.com>
2022-10-22 20:39:47 +0800

    hit

--- assets/utils/request.js
+++ assets/utils/request.js
@@ -4,11 +4,11 @@ axios.defaults.baseURL = 'http://www.itcbc.com:8000';
 axios.interceptors.request.use(function (config) {
     let tokenVal = localStorage.getItem('token')
     if (tokenVal) {
-        axios.defaults.headers.common['Authorization'] = localStorage.getItem('token')
+        config.headers.common['Authorization'] = localStorage.getItem('token')
     }
     // 在发送请求之前做些什么
     return config;
 }, function (error) {
-    // 对请求错误做些什么
+    // 对请求错误做些什么???
     return Promise.reject(error);
 });
