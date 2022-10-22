05c6422fe913fecfb0ef49a60e935a5d3f4ba56f
詹方 <804571770@qq.com>
2022-10-22 12:21:04 +0800

    登录完成

--- assets/js/login.js
+++ assets/js/login.js
@@ -37,11 +37,43 @@ window.onload = function () {
                 toastr.success('res.data.message');
                 if (res.data.code === 0) {
                     gotoLogin.click();
-                    document.querySelector('.register [name="username"]').value = ''
+                    document.querySelector('.register form').reset()
 
                 }
 
             }
         })
     })
+
+
+    $('.login form').bootstrapValidator(test()).on('success.form.bv', function (e) {
+        e.preventDefault();
+        // 通过验证，这里的代码将会执行。我们将Ajax请求的代码放到这里即可
+        let username = document.querySelector('.login [name="username"]').value.trim()
+        let password = document.querySelector('.login [name="password"]').value.trim()
+        // console.log(username, password);
+        // 发送AXIOS请求
+        axios({
+            method: 'post',
+            url: "http://www.itcbc.com:8000/api/login",
+            data: {
+                username,
+                password
+            }
+        }).then(res => {
+            // console.log(res);
+            if (res.status === 200) {
+                toastr.success('res.data.message');
+                if (res.data.code === 0) {
+                    setTimeout(() => {
+                        location.href = './index.html'
+                    }, 1000);
+
+                }
+
+            }
+        })
+
+
+    });
 }
