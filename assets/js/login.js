16cc4ae14e2e0148fe506ce99bc3b36fb82b9f4f
詹方 <804571770@qq.com>
2022-10-22 11:55:44 +0800

    注册页面完成

--- assets/js/login.js
+++ assets/js/login.js
@@ -4,6 +4,9 @@ window.onload = function () {
 
     let register = document.querySelector('.form-box>.register');
     let login = document.querySelector('.form-box>.login ');
+    let loginBtn = document.querySelector('#loginBtn ');
+    let registerBtn = document.querySelector('#registerBtn ');
+
 
     gotoRegister.addEventListener('click', function () {
         login.style.display = 'none'
@@ -13,5 +16,32 @@ window.onload = function () {
     gotoLogin.addEventListener('click', function () {
         login.style.display = 'block'
         register.style.display = 'none'
+    })
+
+    registerBtn.addEventListener('click', function (e) {
+        e.preventDefault()
+        let username = document.querySelector('.register [name="username"]').value.trim()
+        let password = document.querySelector('.register [name="password"]').value.trim()
+        // console.log(username, password);
+
+        axios({
+            method: 'post',
+            url: "http://www.itcbc.com:8000/api/register",
+            data: {
+                username,
+                password
+            }
+        }).then(res => {
+            // console.log(res);
+            if (res.status === 200) {
+                toastr.success('res.data.message');
+                if (res.data.code === 0) {
+                    gotoLogin.click();
+                    document.querySelector('.register [name="username"]').value = ''
+
+                }
+
+            }
+        })
     })
 }
