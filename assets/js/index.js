7be480929809409f4cc49eaa99a2845e26db3ffb
詹方 <804571770@qq.com>
2022-10-22 15:19:51 +0800

    左侧导航栏颜色控制完成

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,14 @@
 
 //侧边导航点击颜色控制
- 
+
+let allA = document.querySelectorAll(".nav>li li a")//获取A标签
+
+allA.forEach(item => {
+    item.addEventListener('click', function () {
+        document.querySelector(".nav>li li a.active").classList.remove("active")
+        this.classList.add('active')
+    })
+})
 
 
 
