e855c55ea555c2df507e467b1b72fba1498f8941
詹方 <804571770@qq.com>
2022-10-22 09:26:04 +0800

    可视化初始化完成

---
+++ assets/utils/toastr.js
@@ -0,0 +1,8 @@
+// ------------------ 提示工具配置 -------------
+
+toastr.options = {
+    hideDuration: "500", // 提示框隐藏渐隐时间
+    timeOut: 2000, // 提示框持续时间
+    positionClass: 'toast-center-center', // 提示框位置（类名）
+	preventDuplicates: true,      //不能显示多个提示框
+}
