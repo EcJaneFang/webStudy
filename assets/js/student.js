18d693e8deb6eeb95d8e852fde867a87610ae5fb
詹方 <804571770@qq.com>
2022-10-24 16:26:06 +0800

    修改数据的回填

--- assets/js/student.js
+++ assets/js/student.js
@@ -1,18 +1,20 @@
 window.onload = function () {
-    axios({
-        method: "get",
-        url: "/student/list",
-        // headers: {
-        //     "Authorization": localStorage.getItem('token')
-        // }
-    }).then(res => {
-        // console.log(res);
-        if (res.status === 200 && res.data.code === 0) {
-            render(res.data.data)
+    function getData() {
+        axios({
+            method: "get",
+            url: "/student/list",
+            // headers: {
+            //     "Authorization": localStorage.getItem('token')
+            // }
+        }).then(res => {
+            // console.log(res);
+            if (res.status === 200 && res.data.code === 0) {
+                render(res.data.data)
 
-        }
-    })
-
+            }
+        })
+    }
+    getData()
     function render(arr) {
         document.querySelector("tbody").innerHTML = arr.map(res => {
             return `<tr>
@@ -71,7 +73,7 @@ window.onload = function () {
                     pname: this.value
                 }
             }).then(res => {
-                console.log(res);
+                // console.log(res);
 
                 if (res.status === 200) {
                     let str = `<option selected="" value="">--市--</option>`
@@ -106,6 +108,7 @@ window.onload = function () {
             })
         })
 
+
         if (e.relatedTarget.innerHTML.trim() === '添加学员') {
             document.getElementById('addModalLabel').innerText = "录入新学员"
             document.getElementById('queren').innerText = "确认添加"
@@ -115,8 +118,196 @@ window.onload = function () {
             document.getElementById('addModalLabel').innerText = "修改学员信息"
             document.getElementById('queren').innerText = "确认修改"
             document.getElementById('chongzhi').style.display = "none"
+            //数据回填
+
+            let id = e.relatedTarget.dataset.id
+            console.log(id);
+            //发送修改的axios亲求
+            axios({
+                method: "",
+                url: "/student/one",
+                params: {
+                    id
+                }
+            }).then(res => {
+                console.log(res);
+                toastr.success(res.data.message);
+                if (res.status === 200 && res.data.code === 0) {
+                    let obj = res.data.data;
+                    //数据回填
+                    document.querySelector('[name="name"]').value = obj.name;
+                    document.querySelector('[name="age"]').value = obj.age;
+
+                    document.querySelector('[name="group"]').value = obj.group;
+
+                    document.querySelector('[name="phone"]').value = obj.phone;
+                    document.querySelector('[name="salary"]').value = obj.salary;
+
+                    document.querySelector('[name="truesalary"]').value = obj.truesalary;
+                    obj.sex === "男" ? (document.querySelectorAll('[name="sex"]')[0].checked) : (document.querySelectorAll('[name="sex"]')[1].checked)
+                    province.value = obj.province
+                    city.innerHTML = ` <option selected value="${obj.city}">${obj.city}</option>`
+                    county.innerHTML = ` <option selected value="${obj.county}">${obj.county}</option>`
+
+
+                }
+            })
         }
 
+        function student() {
+            return {
+                fields: {
+                    name: {
+                        validators: {
+                            notEmpty: {
+                                message: '姓名不能为空',
+                            },
+                            stringLength: {
+                                min: 2,
+                                max: 10,
+                                message: '姓名长度2~10位'
+                            }
+                        }
+                    },
+                    age: {
+                        validators: {
+                            notEmpty: {
+                                message: '年龄不能为空',
+                            },
+                            greaterThan: {
+                                value: 18,
+                                message: '年龄不能小于18岁'
+                            },
+                            lessThan: {
+                                value: 60,
+                                message: '年龄不能超过60岁'
+                            }
+                        }
+                    },
+                    sex: {
+                        validators: {
+                            choice: {
+                                min: 1,
+                                max: 1,
+                                message: '请选择性别'
+                            }
+                        }
+                    },
+                    group: {
+                        validators: {
+                            notEmpty: {
+                                message: '组号不能为空',
+                            },
+                            regexp: {
+                                regexp: /^\d{1,2}$/,
+                                message: '请选择有效的组号'
+                            }
+                        }
+                    },
+                    phone: {
+                        validators: {
+                            notEmpty: {
+                                message: '手机号不能为空',
+                            },
+                            regexp: {
+                                regexp: /^1\d{10}$/,
+                                message: '请填写有效的手机号'
+                            }
+                        }
+                    },
+                    salary: {
+                        validators: {
+                            notEmpty: {
+                                message: '实际薪资不能为空',
+                            },
+                            greaterThan: {
+                                value: 800,
+                                message: '期望薪资不能低于800'
+                            },
+                            lessThan: {
+                                value: 100000,
+                                message: '期望薪资不能高于100000'
+                            }
+                        }
+                    },
+                    truesalary: {
+                        validators: {
+                            notEmpty: {
+                                message: '实际薪资不能为空',
+                            },
+                            greaterThan: {
+                                value: 800,
+                                message: '实际薪资不能低于800'
+                            },
+                            lessThan: {
+                                value: 100000,
+                                message: '实际薪资不能高于100000'
+                            }
+                        }
+                    },
+                    province: {
+                        validators: {
+                            notEmpty: {
+                                message: '省份必填',
+                            },
+                        }
+                    },
+                    city: {
+                        validators: {
+                            notEmpty: {
+                                message: '市必填',
+                            },
+                        }
+                    },
+                    county: {
+                        validators: {
+                            notEmpty: {
+                                message: '县必填',
+                            },
+                        }
+                    },
+                }
+            }
+        }
 
+        // 添加学生
+        $('.add-form').bootstrapValidator(student()).on('success.form.bv', function (e) {
+            e.preventDefault();
+
+            if (document.getElementById("queren").innerText === "确认添加") {
+                let fd = new FormData(document.querySelector('.add-form'))
+                let obj = {}
+                fd.forEach((v, k) => {
+                    obj[k] = v
+                })
+
+                axios({
+                    method: "post",
+                    url: "/student/add",
+                    data: obj
+                }).then(res => {
+                    // console.log(res);
+                    toastr.success(res.data.message)
+                    if (res.status === 200 && res.data.code === 0) {
+                        getData()
+                        $('#addModal').modal('hide')
+
+                    }
+                })
+
+            }
+            //提交逻辑
+            // console.log(222);
+
+
+        });
+
+
+    })
+
+    $('#addModal').on('hidden.bs.modal', function (e) {
+        // do something...
+
+        addModal.querySelector('.add-form').reset();
     })
 }
